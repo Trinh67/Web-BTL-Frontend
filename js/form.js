d80ae95e81416ad33be6d116bdd87717a737bccf
Trinh67 <56555813+Trinh67@users.noreply.github.com>
2020-12-08 00:50:52 +0700

    Validate Email+Password

--- js/form.js
+++ js/form.js
@@ -7,13 +7,3 @@ togglePassword.addEventListener('click', function (e) {
     // toggle the eye slash icon
     this.classList.toggle('fa-eye-slash');
 });
-
-const togglePassword2 = document.querySelector('#togglePassword2');
-const password2 = document.querySelector('#confirm_password');
-togglePassword2.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
-    password2.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('fa-eye-slash');
-});
--- js/register.js
+++ js/register.js
@@ -1,15 +1,64 @@
-var email = $("input#sc_email").val();  
-
-if (email !== "") {  // If something was entered
-    if (!isValidEmailAddress(email)) {
-        $("label#email_error").show(); //error message
-        $("input#sc_email").focus();   //focus on email field
-        return false;  
+var checkEmail = false;
+var checkPassword = false;
+// Check email
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  
+  function validate() {
+      
+    const result = document.getElementById("check-email");
+    var email = document.getElementById("email").value;
+    if (validateEmail(email)) {
+      checkEmail = true;
+      if(checkEmail === true && checkPassword === true) document.getElementById("btnSubmit").disabled = false;
+      result.innerHTML = '<i class="fas fa-check" style="color: green;"></i>';
+    } else {
+      result.innerHTML = '<i class="fas fa-times" style="color: red;"></i>';
     }
-} 
+    return false;
+  }
+  
+document.getElementById("email").onblur = function(){ validate() };
 
-function isValidEmailAddress(emailAddress) {
-    var pattern = new RegExp(/^[A-Za-z0-9]+([_\.\-]?[A-Za-z0-9])*@[A-Za-z0-9]+([\.\-]?[A-Zaz0-9]+)*(\.[A-Za-z]+)+$/);
-    return pattern.test(emailAddress);
-};
+// Show or hide password
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
 
+const togglePassword2 = document.querySelector('#togglePassword2');
+const password2 = document.querySelector('#confirm_password');
+togglePassword2.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+    password2.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+// Check confirm-password
+function comparePassword() {  
+    const pass = document.getElementById("password").value;
+    const confirm = document.getElementById("confirm_password").value;
+    
+    const result = document.getElementById("check-pass");
+    if (pass == confirm && pass != "" && pass.length > 5) {
+      checkPassword = true;
+      if(checkEmail === true && checkPassword === true) document.getElementById("btnSubmit").disabled = false;
+      result.innerHTML = '<i class="fas fa-check" style="color: green;"></i>';
+    } else {
+      result.innerHTML = '<i class="fas fa-times" style="color: red;"></i>';
+    }
+    return false;
+  }
+  
+document.getElementById("confirm_password").onblur = function(){ comparePassword(); };
+
+if(checkEmail === false || checkPassword === false) document.getElementById("btnSubmit").disabled = true;
