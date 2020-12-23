a0a264344b662335fffd49e7e1dfee1b015d539e
Trinh67 <56555813+Trinh67@users.noreply.github.com>
2020-12-24 04:01:24 +0700

    Đăng bài viết mới

--- js/post_sender.js
+++ js/post_sender.js
@@ -1,59 +1,80 @@
 var submitSendPostBt = document.getElementById('postSubmitBtnId');
-var token ={
-    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOlsidHJ1b25nOTExMmsiLCJvd25lciJdLCJuYW1laWQiOiIzNCIsImVtYWlsIjoidGhoQGdtYWlsLmNvbSIsImp0aSI6IjIyNjM2MTkwLWVlZTktNDU0My1hMjg5LTY4NjFhOWZlMjk3ZCIsImV4cCI6MTYwODcyNDYzMywiaXNzIjoiaHV5dHJ1b25nLmNvbSIsImF1ZCI6Imh1eXRydW9uZy5jb20ifQ.X7Nn1p_Jc3sEKE-8wPRZdIEkCx_dojDT_RPmHpYU8wk"
-}
+
+// Kiểm tra dữ liệu đầy đủ chưa
+document.getElementById("postSubmitBtnId").disabled = true;
+$("input").blur(function(){
+  document.getElementById("postSubmitBtnId").disabled = false;
+  for(let i = 0; i < $('.required').length; i++) {
+    if($('.required')[i].value == false)  {document.getElementById("postSubmitBtnId").disabled = true;break;}
+  }
+})
+
+// Thực hiện truy vấn
 submitSendPostBt.onclick = function () {
-    console.log("aaa");
-    SendPost('http://fcbtruong-001-site1.itempurl.com/api/Post/PostUp', token)
-    .then(data => {
-        console.log(data); // JSON data parsed by `data.json()` call
-    });
+  // Lấy danh sách tiện ích
+  var array = [];
+  $("input:checkbox[name=utility-item]:checked").each(function() { 
+      array.push($(this).val()); 
+  }); 
+
+  // Tạo mẫu dữ liệu
+  var infor =  {
+    title: document.querySelector('input[name=title]').value,
+    address: document.querySelector('input[name=address]').value,
+    lat: document.querySelector('input[name=lat]').value,
+    lng: document.querySelector('input[name=lng]').value,
+    price: document.querySelector('input[name=price]').value,
+    area: document.querySelector('input[name=area]').value,
+    district: document.querySelector('select[name=district]').value,
+    category: document.querySelector('select[name=category]').value,
+    phone: document.querySelector('input[name=phone]').value,
+    utilities: array,
+    otherUtilities: document.querySelector('textarea[name=other-utility]').value,
+    description: document.querySelector('textarea[name=description]').value,
+    servicePackage: document.querySelector('select[name=servicePackage]').value,
+    serviceTime: document.querySelector('input[name=serviceTime]').value,
+  }
+  // Tạo Headers
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOlsidHJ1b25nOTExMmsiLCJvd25lciJdLCJuYW1laWQiOiIzNCIsImVtYWlsIjoidGhoQGdtYWlsLmNvbSIsImp0aSI6ImVlZGRmMWZlLTBjYTktNGU4Mi04ZTZmLWFiMmJiOWQxNWE4ZCIsImV4cCI6MTYwODc1MzgwNSwiaXNzIjoiaHV5dHJ1b25nLmNvbSIsImF1ZCI6Imh1eXRydW9uZy5jb20ifQ.q42LBx3q_FWf3LGD1MO2YvAGr2WkDA2ye-VIZXaWo7k");
+  var fileInput = document.getElementById("file-3").files;
+
+  var formdata = new FormData();
+  formdata.append("postStr", JSON.stringify(infor));
+  formdata.append("files", fileInput[0]);
+  formdata.append("files", fileInput[1]);
+  formdata.append("files", fileInput[2]);
+  
+  for(var pair of formdata.entries()) {
+    console.log(pair[0] + ": " +pair[1]);
+  }
+  
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: formdata,
+    redirect: 'follow'
+  };
+
+  fetch("http://fcbtruong-001-site1.itempurl.com/api/Post/PostUp", requestOptions)
+    .then(response => {response.text();SuccessMessage();})
+    .then(result =>
+      console.log(result)
+    )
+    .catch(error => {console.log('error', error);WarningMessage()});
+}
+
+// Thông báo đăng bài thành công
+function SuccessMessage() {
+  $('#alertSuccess').show();
+  $('#alertWarning').hide();
+}
+
+// Thông báo đăng bài bị lỗi
+function WarningMessage() {
+  $('#alertWarning').show();
+  $('#alertSuccess').hide();
 }
 
 
-async function SendPost(url = '', token) {
-    var infor =  {
-        title: "Cho thue nha tro gan DHQG",
-        address: "Ngo 10, Pham Van Dong, Cau Giay",
-        lat: 30.12,
-        lng: 40,
-        price: 4000000,
-        area: 40,
-        district: 2,
-        category: 1,
-        phone: "0983838237",
-        description: "Cho thue gia dan, tien nghi",
-        utilities: [],
-    }
-    //console.log(JSON.stringify)
-    var postForm = new FormData();
-    postForm.append('postStr', JSON.stringify(infor));
 
-    var files = document.getElementById("file-3").files;
-    console.log(files[0].path);
-
-    postForm.append('files', files[0]);
-    // Display the key/value pairs
-for(var pair of postForm.entries()) {
-    console.log(pair[1]);
- }
-
-    // Default options are marked with *
-    const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        headers: {
-            'Content-Type': 'application/json;multipart/form-data',
-            'Authorization': 'Bearer ' + token.token,
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: postForm // body data type must match "Content-Type" header
-    });
-
-    if(response.status == 200){
-        console.log("Post successful");
-    }
-    else {
-        console.log("Failed");
-    };
-    return response.json(); // parses JSON response into native JavaScript objects
-}
