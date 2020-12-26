8e490533d88f7c362e36348fcb4c080c094654dc
Trinh67 <56555813+Trinh67@users.noreply.github.com>
2020-12-26 16:14:24 +0700

    load post
    load post of owner

--- js/admin/admin.js
+++ js/admin/admin.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    loadOwnerRoomData();
     $('#statistic').click(btnStatisticOnClick);
     $('#post-manager').click(btnPostOnclick);
     $('#edit-infor').click(btnEditOnclick);
@@ -9,14 +8,6 @@ $(document).ready(function () {
     $('#log-out').click(btnLogoutOnclick);
     $('#home-Page').click(btnHomePageOnclick);
     $('#notification').click(btnNotificationOnclick);
-    $('#dt-fixed-footer').dataTable({
-        "paging": false,
-        "fnInitComplete": function () {
-          var myCustomScrollbar = document.querySelector('#dt-fixed-footer_wrapper .dataTables_scrollBody');
-          var ps = new PerfectScrollbar(myCustomScrollbar);
-        },
-        "scrollY": 450,
-    });
 })
 // Thống kê
 function btnStatisticOnClick() {
@@ -30,6 +21,15 @@ function btnStatisticOnClick() {
 }
 // Quản lí bài đăng
 function btnPostOnclick() {
+    loadOwnerRoomData();
+    $('#dt-fixed-footer').dataTable({
+        "paging": false,
+        "fnInitComplete": function () {
+          var myCustomScrollbar = document.querySelector('#dt-fixed-footer_wrapper .dataTables_scrollBody');
+          var ps = new PerfectScrollbar(myCustomScrollbar);
+        },
+        "scrollY": 450,
+    });
     $('.main-content').show();
     $('.statistic-content').hide();
     $('.edit-user').hide();
@@ -93,7 +93,7 @@ function btnLogoutOnclick() {
 }
 
 // Tạo bài viết
-let showUtility = function (x, index) {
+let showPost = function (x, index) {
 	// Tạo <tr> và các <td> mới 
 	let r = document.createElement("tr");
 	r.innerHTML = '<td>#' + x[index].post['idPost'] + '</td>' +
@@ -107,7 +107,7 @@ let showUtility = function (x, index) {
                     'href="chi-tiet-phong-tro/'+ x[index].motelInfor['slug'] + '.html"' +
                     'style="color: #055699;">' + x[index].motelInfor['title'] + '</a>' +
                     '<div>' +
-                    '<a href="#" class="btn btn-sm btn-add-day text-success"><i data-feather="plus"></i>+ Thêm ngày</a>' +
+                    '<a href="#" class="btn btn-sm btn-add-day text-success"><i data-feather="plus"></i>+ Thêm ngày</a> ' +
                     '<a href="#" class="btn btn-sm btn-edit-post"><i class="fas fa-edit icon"></i> Sửa</a>' +
                     '<div class="onoffswitch">' +
                         '<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>' +
@@ -119,38 +119,48 @@ let showUtility = function (x, index) {
                     '</div>' +
                     '</td>' +
                     '<td>' + x[index].motelInfor['price'].toLocaleString('it-IT') + ' đ/ tháng</td>' +
-                    '<td>' + formDate(ret[index].post['createdAt']) + '</td>' +
-                    '<td>' + formDate(ret[index].post['expireDate']) + '</td>' +
+                    '<td>' + formDate(x[index].post['createdAt']) + '</td>' +
+                    '<td>' + formDate(x[index].post['expireDate']) + '</td>' +
                     '<td>Gói ngày</td>' +
                     '<td class="badge badge-primary">Chờ phê duyệt</td>';
 	return r;
 };
 
-// Tải dữ liệu phòng trọ của chủ trọ
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOlsidHJ1b25nOTExMmsiLCJvd25lciJdLCJuYW1laWQiOiIzNCIsImVtYWlsIjoidGhoQGdtYWlsLmNvbSIsImp0aSI6IjBlNTg2YWY5LTdmYTItNDg3NC1iMzllLTNlOGNhNmMwZDYwZCIsImV4cCI6MTYwODk3MDg2NCwiaXNzIjoiaHV5dHJ1b25nLmNvbSIsImF1ZCI6Imh1eXRydW9uZy5jb20ifQ.RCTTVsLQuEFU76NFXSRd1ShsJDyPQIJw6ThxeSnhdow");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
 async function loadOwnerRoomData() {
-	await fetch("http://fcbtruong-001-site1.itempurl.com/api/Post/GetPostOfUser")
-	.then(resp => {
-		if (resp.status == 200) {
-			resp.json()
-				.then(ret => {
-                    if (ret != null) {
-						var post = JSON.parse(ret);
-						for (let i = 0; i < post.length; i++) {
-                            let r = showPost(post, i);
-                            console.log(i);
-							document.querySelector("tbody#list-owner-post").appendChild(r);
-                        };
-                        document.querySelector(img.user_avatar).src = '../content/images/avatar/' + ret.owner['avatar'];
-					} else {
-						// Có lỗi xử lý nghiệp vụ
-						alert('Error! Lỗi xử lí nghiệp vụ');
-					}
-				});
-		} else {
-			// Có lỗi HTTP
-			alert('Error! Lỗi HTTP');
-		}
-	});
+    await fetch("http://fcbtruong-001-site1.itempurl.com/api/Post/GetPostOfUser", requestOptions)
+    .then(resp => {
+    if (resp.status == 200) {
+        resp.json()
+            .then(ret => {
+                if (ret != null) {
+                    $('#list-owner-post').empty();
+                    var post = ret;console.log(post);
+                    
+                    for (let i = 0; i < post.length; i++) {
+                        let r = showPost(post, i);
+                        document.querySelector("tbody#list-owner-post").appendChild(r);
+                    };
+                    // document.getElementsByClassName("user_avatar").src = '../../content/images/avatar/' + post['owner']['avatar'];
+                } else {
+                    // Có lỗi xử lý nghiệp vụ
+                    alert('Error! Lỗi xử lí nghiệp vụ');
+                }
+            });
+    } else {
+        // Có lỗi HTTP
+        alert('Error! Lỗi HTTP');
+    }
+})
+  .catch(error => console.log('error', error));
 }
 
 // Format Date
@@ -159,6 +169,5 @@ function formDate(date) {
     day = date.getDate();
     month = date.getMonth() + 1;
     year = date.getFullYear();
-    console.log(date);
     return day + '/' + month + '/' + year;
 }
